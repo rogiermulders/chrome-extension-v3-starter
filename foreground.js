d374d7b6c67e648af12f6165e286026b7ecb6d7b
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-04-10 18:28:46 +0200

    working stuff on c.com

--- foreground.js
+++ foreground.js
@@ -5,3 +5,18 @@
 // and injected into the same or different pages.
 
 console.log("This prints to the console of the page (injected only if the page url matched)")
+
+const chesscomfixer = setInterval(() => {
+  const query = document.querySelectorAll('.game-arc-chart .game-arc-arc')
+
+  if(query.length > 0) {
+    const node = query[0]
+    node.style.filter = 'none'
+    node.style.pointerEvents = 'auto'
+    clearInterval(chesscomfixer)
+  }
+}, 1000)
+
+window.onbeforeunload = function() {
+
+}
